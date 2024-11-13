9e56d4a7279b0ae2e55b457ca9b80cdfac652bb9
Josh <lifetrekkerca@gmail.com>
2024-11-12 18:00:13 -0800

    current lab 6 (not working)

--- artdm174/labs/lab6/j/main.js
+++ artdm174/labs/lab6/j/main.js
@@ -4,6 +4,8 @@ File Name: main.js for Lab 6
 */
 
 
+document.addEventListener('DOMContentLoaded', init);
+
 //This code does NOT create any global variables.
 //Promises can be chained together, with the previous promise
 // passing its results to the next one in the chain.
@@ -11,39 +13,56 @@ File Name: main.js for Lab 6
 //it's easier to read if we put each step in its own line,
 //that's why the periods start the then lines.
 
-fetch("houses.json")
-    .then((response) => response.json())
-    .then((data) => {
-        //create a temp holder to append all the html generated inside the forEach iterator
-        let html = "<dl>";
+function init()
+{
+    fetch("houses.json")
+        .then((response) => response.json())
+        .then((data) => {
+            //create a temp holder to append all the html generated inside the forEach iterator
+            let html = "<dl>";
 
 
-        //the argument "house" passed to the arrow function
-        //holds each item in the array in turn.
-        data.forEach((house) => {
+            //the argument "house" passed to the arrow function
+            //holds each item in the array in turn.
+            data.forEach((house) => {
 
-            // generate the html snippet for one array item
-            //to be added to the "html" temp holder.
-            let objInfo = `<div id="` + house.code + `">  <dt class="house">` + house.name + `</dt>`;
+                // generate the html snippet for one array item
+                //to be added to the "html" temp holder.
+                let objInfo = `<div id="` + house.code + `">  <dt class="house">` + house.name + `</dt>`;
 
-            for(let i = 0; i < house.members.length; i++)
-            {
+                for(let i = 0; i < house.members.length; i++)
+                {
 
-                objInfo += `<dd class="folks">` + house.members[i] + `</dd>`;
-            }
-            
+                    objInfo += `<dd class="folks">` + house.members[i] + `</dd>`;
+                }
+                
 
-            html += objInfo;
-            html += `<img src=images/` + house.name + `Logo.png> </div>`;
+                html += objInfo;
+                html += `<img src=images/` + house.name + `Logo.png> </div>`;
+            });
+
+            html += "</dl>"
+
+            //make a reference to the html container where
+            //the info will be displayed.
+            const container = document.querySelector("#container");
+            container.innerHTML = html;
+        })
+        .catch((err) => console.log("Oops!", err));
+        //this only runs if there is an error during the above process
+
+
+
+    fetch('https://www.colr.org/color/random')
+        .then(response => response.json())
+        .then(color => {
+            console.log(color);
+        })
+        .catch(err => {
+            console.error(err.message);
         });
+  
 
-        html += "</dl>"
 
-        //make a reference to the html container where
-        //the info will be displayed.
-        const container = document.querySelector("#container");
-        container.innerHTML = html;
-    })
-    .catch((err) => console.log("Oops!", err));
-    //this only runs if there is an error during the above process
 
+}
